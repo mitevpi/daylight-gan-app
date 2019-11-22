53df3ffbd3c45402b980d0ef1698e9c8be29d980
Petr Mitev <p.mitevpi@gmail.com>
2019-11-22 12:41:56 -0500

    vuetify added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <transition name="fade">
-      <router-view />
-    </transition>
+    <v-app>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <transition name="fade">
+        <router-view />
+      </transition>
+    </v-app>
   </div>
 </template>
 
