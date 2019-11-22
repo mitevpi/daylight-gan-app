3c996207455605f35a3aae3b63a696686deeddcd
Petr Mitev <p.mitevpi@gmail.com>
2019-11-22 12:11:57 -0500

    click to draw square working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br >
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-        >babel</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-        >router</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-        >vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-        >eslint</a>
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li>
-        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
-      </li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
-      </li>
-    </ul>
+  <div id="container" class="svg-container" align="center">
+    <svg v-if="redrawToggle === true" ref="svg" :width="svgWidth" :height="svgHeight">
+      <g>
+        <rect
+          v-for="item in dataSet"
+          :key="item.x"
+          fill="green"
+          :x="item.x"
+          :y="item.y"
+          :width="50"
+          :height="50"
+        />
+      </g>
+    </svg>
   </div>
 </template>
 
 <script>
+// import { scaleLinear, scaleBand } from "d3-scale";
+// import { event } from "d3-selection";
+// import { transition } from "d3-transition";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
-  }
+  },
+  data: () => ({
+    dataSet: [{ x: 0, y: 0 }],
+    svgWidth: 0,
+    redrawToggle: true,
+    svg: null,
+    mouseX: null,
+    mouseY: null
+  }),
+  computed: {
+    svgHeight() {
+      return this.svgWidth / 1.61803398875; // golden ratio
+    }
+  },
+  mounted() {
+    this.svgWidth = document.getElementById("container").offsetWidth * 0.75;
+    this.svg = this.$refs.svg;
+    this.svg.addEventListener("mousemove", e => {
+      const dim = e.target.getBoundingClientRect();
+      // console.log(e.clientX, e.clientY); // log the mouse x,y position
+      this.mouseX = e.clientX - dim.left;
+      this.mouseY = e.clientY - dim.top;
+    });
+
+    this.svg.addEventListener("click", () => {
+      this.dataSet.push({ x: this.mouseX, y: this.mouseY });
+    });
+  },
+  methods: {}
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.bar-positive {
+  fill: steelblue;
+  transition: r 0.2s ease-in-out;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.bar-positive:hover {
+  fill: brown;
 }
-li {
+.svg-container {
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  position: relative;
+  width: 100%;
+  padding-bottom: 1%;
+  vertical-align: top;
+  overflow: hidden;
 }
 </style>
